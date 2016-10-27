b4c2269670f576f2b6aaa3e7e9d3fd003e40b943
Pedro Contipelli <Pcontipelli10@hotmail.com>
2016-10-27 00:13:05 -0400

    Add files via upload

---
+++ simplicity.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class simplicity {
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+
+String alpha = "abcdefghijklmnopqrstuvwxyz";
+int[] nums = new int[26]; 
+
+String str = scan.next();
+
+for (int i = 0; i < str.length(); i++)
+	nums[alpha.indexOf(str.charAt(i))]++;
+
+Arrays.sort(nums);
+
+int sum = 0;
+
+for (int i = 0; i < nums.length - 2; i++)
+	sum += nums[i];
+
+System.out.println(sum);
+
+scan.close();
+	}
+}
