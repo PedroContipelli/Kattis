a171fa45fed638416a76e75a4c054c5ab8031b47
Pedro Contipelli <Pcontipelli10@hotmail.com>
2016-11-24 17:15:15 -0500

    Add files via upload

--- tracksmoothing.java
+++ tracksmoothing.java
@@ -3,9 +3,9 @@ public class tracksmoothing {
 public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
 
-int testcases = scan.nextInt();
+int cases = scan.nextInt();
 
-for (int zax = 0; zax < testcases; zax++)
+while (cases --> 0)
 	{
 	int radius = scan.nextInt();
 	int numPoints = scan.nextInt();
@@ -21,25 +21,23 @@ for (int zax = 0; zax < testcases; zax++)
 		y[i] = scan.nextInt();
 		}
 	
-	for (int i = 0; i < numPoints - 1; i++)
-		{
-		int dY = y[i + 1] - y[i];
-		int dX = x[i + 1] - x[i];
-		
-		trackLength += Math.sqrt(dY*dY + dX*dX);
-		}
-	
 	int dY = y[0] - y[numPoints - 1];
 	int dX = x[0] - x[numPoints - 1];
 	
-	trackLength += Math.sqrt(dY*dY + dX*dX);
+	trackLength += Math.hypot(dX , dY);
+	
+	for (int i = 0; i < numPoints - 1; i++)
+		{
+		dY = y[i + 1] - y[i];
+		dX = x[i + 1] - x[i];
+		
+		trackLength += Math.hypot(dY , dX);
+		}
 	
 	double circumference = 2*radius*Math.PI;
 	
 	if (trackLength < circumference)
 		System.out.println("Not possible");
-	else if (trackLength == circumference)
-		System.out.println(0);
 	else
 		{
 		System.out.printf("%.6f", (trackLength - circumference)/trackLength);
