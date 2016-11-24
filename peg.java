77b5b145f56cfc41b6d5fe55250e6101b6e3bea2
Pedro Contipelli <Pcontipelli10@hotmail.com>
2016-11-24 17:45:55 -0500

    Add files via upload

--- peg.java
+++ peg.java
@@ -3,64 +3,53 @@ public class peg {
 	
 public static int moves(int r , int c , char[][] nums) {
 	int move = 0;
+	
 	if (nums[r][c] == '.')
 		return 0;
+	
 	if (nums[r + 1][c] == 'o' && nums[r + 2][c] == '.')
 		move++;
+	
 	if (nums[r - 1][c] == 'o' && nums[r - 2][c] == '.')
 		move++;
+	
 	if (nums[r][c + 1] == 'o' && nums[r][c + 2] == '.')
 		move++;
+	
 	if (nums[r][c - 1] == 'o' && nums[r][c - 2] == '.')
 		move++;
+	
 	return move;
 }
 
 public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
 
-char[][] nums = new char[11][11];
+char[][] peg = new char[11][11];
 int sum = 0;
 
-for (int r = 0; r < 11; r++)
-	for (int c = 0; c < 11; c++)
-		nums[r][c] = 'o';
+for (int i = 0; i < peg.length; i++)
+	peg[i] = "ooooooooooo".toCharArray();
 
-for (int r = 2; r < 9; r++)
+for (int r = 2; r <= 8; r++)
 	{
 	String str = scan.next();
-		if (r <= 3 || r >= 7)
-			{
-			for (int i = 4; i < 7; i++)
-				nums[r][i] = str.charAt(i - 4);
-			}
-		else
-			{
-			for (int i = 2; i < 9; i++)
-				nums[r][i] = str.charAt(i - 2);
-			}
+	
+	if (str.length() == 3)
+		str = "oooo" + str + "oooo";
+	else
+		str = "oo" + str + "oo";
+	
+	peg[r] = str.toCharArray();
 	}
 
-for (int r = 2; r < 9; r++)
-{
-	if (r <= 3 || r >= 7)
-		{
-		for (int i = 4; i < 7; i++)
-			sum += moves(r , i , nums);
-		}
-	else
-		{
-		for (int i = 2; i < 9; i++)
-			sum += moves(r , i , nums);
-		}
-}
+for (int r = 2; r <= 8; r++)
+	for (int c = 2; c <= 8; c++)
+		if ((r >= 4 && r <= 6) || (c >= 4 && c <= 6))
+			sum += moves(r , c , peg);
 
 System.out.println(sum);
-		
-
-
 
 scan.close();
 	}
-
 }
