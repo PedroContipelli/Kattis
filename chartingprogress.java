fa23283f7cd98598143972263e1deef491fb82a7
Pedro Contipelli <Pcontipelli10@hotmail.com>
2017-01-23 15:28:19 -0500

    Add files via upload

--- chartingprogress.java
+++ chartingprogress.java
@@ -4,47 +4,36 @@ public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
 
 while (scan.hasNext())
-	{
-	ArrayList<String> graph = new ArrayList<>();
-	
-	while (true)
-		{
-		String row = scan.nextLine();
-		
-		if (row.isEmpty())
-			break;
-		
-		graph.add(row);
-		
-		if (!scan.hasNext())
-			break;
-		}
-	
-	int pos = graph.get(0).length() - 1;
-	
-	for (String row : graph)
-		{
-		int length = row.length();
-		int asterisks = 0;
-		
-		for (int i = 0; i < length; i++)
-			if (row.charAt(i) == '*')
-				asterisks++;
-		
-		char[] newRow = row.replace("*" , ".").toCharArray();
-		
-		for (int i = 0; i < asterisks; i++)
-			{
-			newRow[pos] = '*';
-			pos--;
-			}
-		
-		System.out.println(new String(newRow));
-		}
-	
-	System.out.println();
-	}
+    {
+    ArrayList<String> graph = new ArrayList<>();
+    
+    while (scan.hasNext())
+        {
+        String row = scan.nextLine();
+
+        if (row.isEmpty())
+            break;
+        
+        graph.add(row);
+        }
+    
+    int total = 0;
+    
+    for (String row : graph)
+        {
+    	char[] log = row.replace("*" , "..").toCharArray();
+    	
+    	int stars = log.length - row.length();
+        
+    	for (int i = 0; i < stars; i++)
+    		log[row.length() - 1 - total++] = '*';
+        
+        System.out.println(new String(log).substring(0 , row.length()));
+        }
+    
+    System.out.println();
+    }
 
 scan.close();
-	}
+    }
 }
