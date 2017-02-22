c40919e8495a7528309d7e7ff26b9a5c32f0eb90
Pedro Contipelli <Pcontipelli10@hotmail.com>
2017-02-22 12:33:15 -0500

    Add files via upload

--- 2048.java
+++ 2048.java
@@ -1,120 +1,64 @@
-import java.util.Scanner;
+import java.util.*;
 public class 2048 {
 	
-static int[] nums = new int[4];
-
-public static void move() {
+public static void move(int[] nums) {
+	
 	boolean[] merge = new boolean[4];
-
-	for (int z = 1; z < nums.length; z++)
-		{
-		int cell = nums[z];
-		int x = z;
-		
-		while(true)
+	
+	for (int i = 0; i < nums.length; i++)
+		for (int j = i; j - 1 >= 0 && !merge[j - 1]; j--)
 			{
-			
-			if (x == 0)
-				break;
-			
-			if (nums[x - 1] == 0)
+			if (nums[j - 1] == 0)
 				{
-				nums[x] = 0;
-				nums[x - 1] = cell;
-				x -= 1;
+				nums[j - 1] = nums[j];
+				nums[j] = 0;
 				}
-			else if (merge[x - 1] || nums[x - 1] != cell)
+			else if (nums[j - 1] == nums[j])
 				{
+				nums[j - 1] *= 2;
+				nums[j] = 0;
+				merge[j - 1] = true;
 				break;
 				}
 			else
-				{
-				nums[x] = 0;
-				nums[x - 1] *= 2;
-				merge[x - 1] = true;
-				}
+				break;
 			}
-		}
+}
+
+public static int[][] rotate(int[][] nums) {
+	
+	int[][] rot = new int[4][4];
+	
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++)
+			rot[3 - j][i] = nums[i][j];
+	
+	return rot;
 }
 
 public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
-
-int[][] board = new int[4][4];
-
-for (int r = 0; r < 4; r++)
-	for (int c = 0; c < 4; c++)
-		board[r][c] = scan.nextInt();
-
-int direction = scan.nextInt();
-
-//0 left
-//1 up
-//2 right
-//3 down
-
-if (direction == 0)
-	{
-	for (int r = 0; r < 4; r++)
-		{
-		for (int i = 0; i < 4; i++)
-			nums[i] = board[r][i];
-		
-		move();
-		
-		for (int i = 0; i < 4; i++)
-			board[r][i] = nums[i];
-		}
-	}
-else if (direction == 1)
-	{
-	for (int c = 0; c < 4; c++)
-		{
-		for (int i = 0; i < 4; i++)
-			nums[i] = board[i][c];
-		
-		move();
-		
-		for (int i = 0; i < 4; i++)
-			board[i][c] = nums[i];
-		}
-	}
-else if (direction == 2)
-	{
-	for (int r = 0; r < 4; r++)
-		{
-		for (int i = 3; i >= 0; i--)
-			nums[i] = board[r][3 - i];
-		
-		move();
-		
-		for (int i = 0; i < 4; i++)
-			board[r][3 - i] = nums[i];
-		}
-	}
-else
-	{
-	for (int c = 0; c < 4; c++)
-		{
-		for (int i = 3; i >= 0; i--)
-			nums[i] = board[3 - i][c];
-		
-		move();
-		
-		for (int i = 0; i < 4; i++)
-			board[3 - i][c] = nums[i];
-		}
-	}
-
-for (int r = 0; r < 4; r++)
-	{
-	for (int c = 0; c < 4; c++)
-		System.out.print(board[r][c] + " ");
 	
-	System.out.println();
-	}
+int[][] nums = new int[4][4];
+
+for (int i = 0; i < 4; i++)
+	for (int j = 0; j < 4; j++)
+		nums[i][j] = scan.nextInt();
+
+int move = scan.nextInt();
+
+for (int i = 0; i < move; i++)
+	nums = rotate(nums);
+	
+for (int i = 0; i < 4; i++)
+	move(nums[i]);
+
+for (int i = 0; i < 4 - move; i++)
+	nums = rotate(nums);
+
+for (int i = 0; i < 4; i++)
+	System.out.println(Arrays.toString(nums[i]).substring(1 , Arrays.toString(nums[i]).length() - 1).replace("," , ""));
 
 scan.close();
 	}
-
 }
